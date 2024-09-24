234650d909d4c14d59f7378529955499e9e262e5
FloJouff <fenryll01@gmail.com>
2024-09-24 11:22:25 +0200

    recupération static

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -126,7 +126,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
+WHITENOISE_MANIFEST_STRICT = False
 
 sentry_sdk.init(
     dsn=SENTRY,
