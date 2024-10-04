a95e41856f971b35fb5b6615482ba9b79d7ef483
FloJouff <fenryll01@gmail.com>
2024-10-04 09:54:32 +0200

    correction ReadtheDocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,26 +1,14 @@
 import os
 import sys
 
-from unittest.mock import MagicMock
+import django
 
 sys.path.insert(0, os.path.abspath('../..'))
 
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oc_lettings_site.settings")
 
-
-MOCK_MODULES = ['django', 'django.conf', 'django.db', 'django.db.models',
-                'django.contrib', 'django.contrib.auth',
-                'django.contrib.auth.models', 'django.shortcuts', 'django.http', 'sentry_sdk']
-
-
-for mod_name in MOCK_MODULES:
-    sys.modules[mod_name] = MagicMock()
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
+django.setup()
 
 # Configuration file for the Sphinx documentation builder.
 #
@@ -41,19 +29,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
     ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'django': ('https://docs.djangoproject.com/en/stable/',
-               'https://docs.djangoproject.com/en/stable/_objects/'),
-}
 
 templates_path = ['_templates']
 exclude_patterns = []
-autodoc_mock_imports = ["django", "oc_lettings_site", "lettings", "profiles",
-                        "django.shortcuts", "django.core.exceptions"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
